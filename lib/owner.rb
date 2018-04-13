af92d0b45162364ed049da7aad5b505d71863944
Catherine Salazar <catcreates13@gmail.com>
2018-04-13 01:03:17 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -34,7 +34,11 @@ class Owner
   def buy_fish(name)
     new_pet = Fish.new(name)
     @pets[:fishes] << new_pet
+  end
 
+  def buy_cat(name)
+    new_pet = Cat.new(name)
+    @pets[:cats] << new_pet
   end
 
 end
