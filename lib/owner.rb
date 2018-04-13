4bcfce7c984f0babd7428044947dd8018cf3cf33
Catherine Salazar <catcreates13@gmail.com>
2018-04-13 00:29:01 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,17 +2,16 @@ require 'pry'
 
 class Owner
 
-    attr_accessor :name
-    attr_reader :species
+
 
 @@all =[]
 
-  def initialize(name)
-    @name = name
-    @@all << self
-    @@species = "human"
+def self.all
+  @@all
 
-  end
+end
+
+
 
 
 end
